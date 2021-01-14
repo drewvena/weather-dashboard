2a8d6d7c02f3442f03dc63c78f36189b477540b5
Drew Vena <drewvena1@gmail.com>
2021-01-14 16:52:27 -0600

    moved files to proper directories

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,14 +2,14 @@ myFunction=function() {
     var inputEl = document.getElementById("city-input");
     var searchEl = document.getElementById("search-button");
     var clearEl = document.getElementById("clear-history");
-
+    
     var historyEl = document.getElementById("history");
     var searchHistory = JSON.parse(localStorage.getItem("search")) || [];
     console.log(searchHistory);
     
     var APIkey = "5bbac74e67189da529f39b7f6dbd81ba";
     function getWeather(cityName) {
-      
+
                  fetch( "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIkey)
                 
                 .then(function(response){
